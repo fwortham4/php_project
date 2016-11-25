c8c81c3ac37e9ed66e26406ff00b03ae2a49bf07
fwortham4 <forrest.wortham@gmail.com>
2016-11-25 11:09:50 -0600

    Successfully able to run php scripts in browsers, and adds CSS styling.

--- index.php
+++ index.php
@@ -3,26 +3,34 @@
 <head>
   <meta charset="UTF-8">
   <title>Forrest's Project</title>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light+Two" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light+Two" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="CSS/main.css" type="text/css">
 
   <meta name="viewport" content="width=width-device, initial-scale=1.0">
 </head>
 <body>
   <div class="list">
-    <h1 class="header"To do.></h1>
+    <h1 class="header">To do.</h1>
 
-  <ul>
-    <li><span class="item done">Learn PHP</span></li>
-    <li><span class="item"></span>Get a Job</li>
-    <li><span class="item"></span>Be Awesome</li>
-  </ul>
+    <ul class="items">
+      <li>
+        <span class="item done">Learn PHP</span>
+      </li>
+      <li>
+        <span class="item">Get a Job</span>
+        <a href="#" class="done-button">Mark as Done</a>
+      </li>
+      <li>
+        <span class="item">Be Awesome</span>
+        <a href="#" class="done-button">Mark as Done</a>
+      </li>
+    </ul>
 
-  <form class="item-add" action="add.php" method="POST">
-    <input type="text" name="name" placeholder="Add to the to do list HERE." class="input" autocomplete="off" required>
-    <input type="submit" value="Add Item" class="submit">
-  </form>
+    <form class="item-add" action="add.php" method="POST">
+      <input type="text" name="name" placeholder="Add to the to do list HERE." class="input" autocomplete="off" required>
+      <input type="submit" value="Add Item" class="submit">
+    </form>
 
   </div>
 
