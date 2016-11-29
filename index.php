c3beab9fd3431b36632b1e14ee716a24526d2440
fwortham4 <forrest.wortham@gmail.com>
2016-11-29 13:22:12 -0600

    Renames add and mark php files.

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Forrest's Project</title>
+  <title>Forrestifiya</title>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light+Two" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="CSS/main.css" type="text/css">
@@ -43,9 +43,9 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
         <li>
           <span class="item<?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']; ?></span>
           <?php if(!$item['done']): ?>
-            <a href="mark.php?as=done&item=<?php echo $item['id'];  ?>" class="done-button">Mark as In Compliance</a>
+            <a href="mark_item.php?as=done&item=<?php echo $item['id'];  ?>" class="done-button">Mark as In Compliance</a>
           <?php elseif($item['done']): ?>
-            <a href="mark.php?as=notdone&item=<?php echo $item['id'];  ?>" class="done-button">Unmark</a>
+            <a href="mark_item.php?as=notdone&item=<?php echo $item['id'];  ?>" class="done-button">Unmark</a>
           <?php endif; ?>
         </li>
       <?php endforeach; ?>
@@ -54,7 +54,7 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
       <p>No Compliance Items are Currently Listed!</p>
     <?php endif; ?>
 
-    <form class="item-add" action="add.php" method="POST">
+    <form class="item-add" action="add_item.php" method="POST">
       <input type="text" name="name" placeholder="Add an Item to the List." class="input" autocomplete="off" required>
       <input type="submit" value="Add Item" class="submit">
     </form>
